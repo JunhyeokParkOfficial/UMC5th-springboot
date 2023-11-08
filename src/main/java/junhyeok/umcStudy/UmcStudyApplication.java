9fade05e69cadb34aa9d29a79e1f016c189b5c8a
junhyeok <junhyeok0246@naver.com>
2023-11-08 21:17:48 +0900

    Initial commit

---
+++ src/main/java/junhyeok/umcStudy/UmcStudyApplication.java
@@ -0,0 +1,13 @@
+package junhyeok.umcStudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmcStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmcStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/junhyeok/umcStudy/UmcStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package junhyeok.umcStudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmcStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
