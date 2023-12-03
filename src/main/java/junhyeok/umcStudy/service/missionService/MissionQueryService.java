72cff832d4789956bde4b98f5ecbfb7309dcde6f
junhyeok <junhyeok0246@naver.com>
2023-12-03 21:55:54 +0900

    Feat : 내가 진행중인 미션 목록 API 구현

--- src/main/java/junhyeok/umcStudy/service/missionService/MissionQueryService.java
+++ src/main/java/junhyeok/umcStudy/service/missionService/MissionQueryService.java
@@ -1,8 +1,12 @@
 package junhyeok.umcStudy.service.missionService;
 
 import junhyeok.umcStudy.domain.Mission;
+import junhyeok.umcStudy.domain.mapping.MemberMission;
 import org.springframework.data.domain.Page;
+
 
 public interface MissionQueryService {
     Page<Mission> getMissionByStore(Long storeId, Integer page);
+
+    Page<MemberMission> getMissionByMember(Long memberId, Integer page);
 }
